22acc6fc5a5206d2f2e384e6d06c108023424baf
YannDnl <yanndaniel@orange.fr>
2025-02-04 18:20:44 +0100

    changed run

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ p = 200
 m_sur_d_min = 0
 m_sur_d_max = 2
 n_points = 5
-k_points = 5
-l_points = 6
+k_points = 10
+l_points = 12
 
 #simulation.plotPsiDot(m, d, q, p)
 #simulation.plotCvsM(m_sur_d_min, m_sur_d_max, q, p, n_points)
